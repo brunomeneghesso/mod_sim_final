cf0cf538ca9f11577d1d0982955084654e0fdfa0
brunomeneghesso <brunomeneghesso@gmail.com>
2019-05-28 12:00:48 -0300

    fiz o codigo

---
+++ codigo.py
@@ -0,0 +1,58 @@
+import math
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import numpy as np
+ro=1.225
+m=8.03506
+g=9.8
+cd=0.295
+a=4.5**2*math.pi
+def iniciais(v,teta):
+    return [0,1.6,math.cos(teta)*v, math.sin(teta)*v]
+def derivadas (condicoes,t):
+    x=condicoes[0]
+    y=condicoes[1]
+    vx=condicoes[2]
+    vy=condicoes[3]
+    dx=vx
+    if y > 0:
+        dy=vy
+    else:
+        dy=0
+    dvx=-1/2*ro*cd*a*vx/(vx**2+vy**2)**(1/2)/m
+    dvy=1/2*ro*cd*a*vy/(vx**2+vy**2)**(1/2)/m-g
+    return [dx,dy,dvx,dvy]
+listaT = np.arange(0,1,0.001)
+angulo=np.arange(0,math.pi/2,0.05)
+v0=100
+solucoes=[]
+for a in angulo:
+    ci = iniciais(v0,a)
+    solucoes.append(odeint(derivadas,ci,listaT))
+for s in solucoes:
+    plt.plot(s[:,0],s[:,1])
+plt.grid(True)
+plt.show()
+for s in solucoes:
+    plt.plot(listaT,s[:,2])
+plt.grid(True)
+plt.show()
+for s in solucoes:
+    plt.plot(listaT,s[:,3])
+plt.grid(True)
+plt.show()
+altura30=[]
+for s in solucoes:
+    c=0
+    while c<len(s[:,1]) and s[:,1][c] < 30:
+        c+=1
+    if c<len(s[:,0]):
+        altura30.append(s[:,0][c])
+angulos=[]
+for a in angulo:
+    angulos.append(a)
+while len(angulos)>len(altura30):
+    del angulos[len(altura30)]
+plt.plot(angulos,altura30)
+plt.grid(True)
+plt.show()
